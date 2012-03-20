1e40031912edea86b519731d5e825ba7775d41e6
Joel Stoker <truesilver92@gmail.com>
2012-03-20 20:54:05 +0000

    Initial send of code. This is really just the skeleton of how the different types will interact.

---
+++ ButtKickingAlieGame.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buttkickingaliengame;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+/**
+ *
+ * @author ubuntu
+ */
+public class ButtKickingAlieGame 
+{
+    
+    
+    public static void gameLoop() throws InterruptedException
+    {
+        
+        
+        
+        
+//        int x = 10;
+//        int y = 10;
+//        int width = 10;
+//        int height = 10;
+//        
+//        //g.clearRect(x, y, width, height);
+//        x *= 1.1;
+//        y *= 1.1;
+//        width *= 1.2;
+//        height *= 1.2;
+//        g.drawRect(x, y, width, height);
+//        //Thread.sleep(500);
+//        //ui.canvas().repaint();
+        
+        
+    }
+    
+    
+}
---
+++ HandlingEvents.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buttkickingaliengame;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.JFrame;
+ 
+public class HandlingEvents extends Canvas {
+ 
+    int myX = 400;
+    int myY = 400;
+ 
+    public HandlingEvents() {
+        setSize(new Dimension(500, 500));
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent evt) {
+                moveIt(evt);
+            }
+        });
+    }
+ 
+    public void paint(Graphics g) {
+        g.fillOval(myX, myY, 30, 30);
+    }
+ 
+    public void moveIt(KeyEvent evt) {
+     switch (evt.getKeyCode()) {
+            case KeyEvent.VK_DOWN:
+                myY += 5;
+                break;
+            case KeyEvent.VK_UP:
+                myY -= 5;
+                break;
+            case KeyEvent.VK_LEFT:
+                myX -= 5;
+                break;
+            case KeyEvent.VK_RIGHT:
+                myX += 5;
+                break;
+        }
+ 
+        repaint();
+    }
+ 
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Basic Game");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        HandlingEvents ex = new HandlingEvents();
+        frame.getContentPane().add(ex);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setVisible(true);
+        ex.requestFocus();
+    }
+}
